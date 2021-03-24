28cb3a7497b6160eb5ec4546fd763b95d1bf4de2
vitoananda <vtoananda@gmail.com>
2021-03-24 20:30:24 +0700

    testingpy

---
+++ VCODEPython/Inputoutput.py
@@ -0,0 +1,33 @@
+#output
+a = 5
+print(a)
+
+#input
+nama = input("masukan nama :")
+print("nama = ",nama)
+
+#operasisederhana
+b = 5
+c = 5
+penjumlahan = b+c
+perkalian = b*c
+pembagian = b/c
+pemangkatan = b**c
+print("Hasil Penjumlahan",penjumlahan)
+print("Hasil Perkalian",perkalian)
+print("Hasil Pembagian",pembagian)
+print("Hasil Pemangkatan",pemangkatan)
+
+#inputoperasisederhana
+raw_d = input("Masukan d:")
+raw_e = input("Masukan e:")
+d = int(raw_d)
+e = int(raw_e)
+jmlhinput = d+e
+kaliinput = d*e
+bagiinput = d/e
+pangkatinput = d**e
+print("Hasil Penjumlahan",jmlhinput)
+print("Hasil Perkalian",kaliinput)
+print("Hasil Pembagian",bagiinput)
+print("Hasil Pemangkatan",pangkatinput)
